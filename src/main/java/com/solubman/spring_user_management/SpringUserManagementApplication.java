aa4cb28da6a8dbbb2aa7585b7b6f32edfde132de
SolomonBekele <solubman28@gmail.com>
2024-01-22 08:48:30 +0300

    initial

---
+++ src/main/java/com/solubman/spring_user_management/SpringUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.solubman.spring_user_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solubman/spring_user_management/SpringUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solubman.spring_user_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
